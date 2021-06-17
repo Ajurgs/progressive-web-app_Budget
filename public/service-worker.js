9527756fb2f71487dbaa5687dcf03d7c8021be05
Ajurgs <ajjurgs5954@gmail.com>
2021-06-17 15:51:26 -0600

    fixed the capitalization in the icon refrence in files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,14 @@
 
 
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./index.js",
-    "./db.js",
-    "./manifest.json",
-    "./styles.css",
-    "./icons/icon-192X192.png",
-    "./icons/icon-512X512.png",
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ]
 
 const CACHE_NAME = "static-cache-v1";
