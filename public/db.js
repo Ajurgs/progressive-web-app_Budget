d942fe4cc79d8329f99049213775937618263002
Ajurgs <ajjurgs5954@gmail.com>
2021-06-16 20:24:00 -0600

    added the info to the manifest

--- public/db.js
+++ public/db.js
@@ -3,35 +3,38 @@ const { response } = require("express");
 let db;
 
 const request = window.indexedDB.open("BudgetDB",1);
-
+// on upgradeNeeded
 request.onupgradeneeded = function (e){
     db= e.target.result;
     db.createObjectStore("BudgetStore",{keyPath:"ID", autoIncrement:true})
 }
-
+// on Success
 request.onsuccess = function (e){
     db = e.target.result;
     if(navigator.onLine){
         checkDatabase();
     }
 }
+// on error
 request.onerror = function(event){
     console.log(event.target.errorCode);
 }
-
+// save a record to the database
 function saveRecord(record){
     const transaction = db.transaction(["BudgetStore"],"readwrite");
     const budgetStore = transaction.objectStore("BudgetStore");
     budgetStore.add(record);
 }
-
+// check the local database 
 function checkDatabase(){
     const transaction = db.transaction(["BudgetStore"],"readwrite");
     const budgetStore = transaction.objectStore("BudgetStore");
+    // get all the entries
     const getAll = budgetStore.getAll();
 
     getAll.onsuccess = function(){
         if(getAll.result.length >0){
+            // send the local data to the server database
             fetch("/api/transaction/bulk", {
                 method: "POST",
                 body: JSON.stringify(getAll.result),
@@ -41,6 +44,7 @@ function checkDatabase(){
                 }
             }).then((response) => response.json())
             .then(() =>{
+                // clear the local database
                 const transaction = db.transaction(["BudgetStore"],"readwrite");
                 const budgetStore = transaction.objectStore("BudgetStore");
                 const cursorRequest = budgetStore.openCursor();
